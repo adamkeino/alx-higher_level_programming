da79746224a2e134cd5a27cd9ac0cca26281272b
Adam Keino <keinoadam@gmail.com>
2022-06-06 14:57:10 +0000

    why?

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    ans = 0
-    if len(my_list) == 0:
-        return None
-    
-    for i in range(0, (len(my_list)-1)):
-        if my_list[i] > ans:
-            ans = my_list[i]
-
-    return ans
+def max_integer(my_list=[]): 
+    if my_list:
+        ans = 0
+        for i in range(0, (len(my_list)-1)):
+            if my_list[i] > ans:
+                ans = my_list[i]
+        return ans
+    return None
