48dc78003dcd9d9b29a6574da7f0718616355c99
Adam Keino <keinoadam@gmail.com>
2022-06-06 14:05:34 +0000

    why?

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,31 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_b) == 0:
-        temp = list(tuple_b)
-        temp.append(0)
-        temp.append(0)
-        tuple_b = tuple(temp)
-    elif len(tuple_b) == 1:
-        temp = list(tuple_b)
-        temp.append(0)
-        tuple_b = tuple(temp)
-    elif len(tuple_a) == 0:
-        temp = list(tuple_a)
-        temp.append(0)
-        temp.append(0)
-        tuple_a = tuple(temp)
-    elif len(tuple_a) == 1:
-        temp = list(tuple_a)
-        temp.append(0)
-        tuple_a = tuple(temp)
-    elif len(tuple_a) >= 2:
-        temp_a = list(tuple_a)
-        temp_a = temp_a[0:2]
-        tuple_a = tuple(temp_a)
-    elif len(tuple_b) >= 2:
-        temp_b = list(tuple_b)
-        temp_b = temp_b[0:2]
-        tuple_b = tuple(temp_b)
-    
-    sum_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return sum_tuple
+    a, b = len(tuple_a), len(tuple_b)
+    new_tuple = ((tuple_a[0] if a >= 1 else 0) + (tuple_b[0] if b >= 1 else 0),
+                 (tuple_a[1] if a >= 2 else 0) + (tuple_b[1] if b >= 2 else 0))
+    return new_tuple
