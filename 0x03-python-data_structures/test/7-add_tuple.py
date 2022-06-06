8986f6fc5eeaf8a3e06126acc787d222782ad25f
Adam Keino <keinoadam@gmail.com>
2022-06-06 14:03:40 +0000

    very naive

--- 0x03-python-data_structures/test/7-add_tuple.py
+++ 0x03-python-data_structures/test/7-add_tuple.py
@@ -18,6 +18,14 @@ def add_tuple(tuple_a=(), tuple_b=()):
         temp = list(tuple_a)
         temp.append(0)
         tuple_a = tuple(temp)
+    elif len(tuple_a) >= 2:
+        temp_a = list(tuple_a)
+        temp_a = temp_a[0:2]
+        tuple_a = tuple(temp_a)
+    elif len(tuple_b) >= 2:
+        temp_b = list(tuple_b)
+        temp_b = temp_b[0:2]
+        tuple_b = tuple(temp_b)
     
     sum_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return sum_tuple
