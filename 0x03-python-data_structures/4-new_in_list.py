903b9764903644abb502be2d5985875dac75549d
Adam Keino <keinoadam@gmail.com>
2022-06-06 07:20:24 +0000

    t2

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     new = my_list[:]
     if idx < 0 or idx >= len(my_list):
-        return new
-    else:
-        my_list[idx] = element
         return my_list
+    else:
+        new[idx] = element
+        return new
