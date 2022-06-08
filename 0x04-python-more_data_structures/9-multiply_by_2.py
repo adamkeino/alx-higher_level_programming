bfd4b330783d8a9d175ad90065a41bb0fa3bc429
Adam Keino <keinoadam@gmail.com>
2022-06-08 17:27:52 +0000

    dict

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dict = {}
-    for i in a_dictionary:
-        new_dict[i]
+    if a_dictionary:
+        for i in a_dictionary:
+            new_dict[i]
     return new_dict
