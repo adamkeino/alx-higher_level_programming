2c336f5d6698a328b607922e94342c72b57a675d
Adam Keino <keinoadam@gmail.com>
2022-06-20 09:12:36 +0000

    dissapointing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        p = ''.join(map(str, my_list[:x]))
-        print("{}".format(p))
-    except:
-        pass
-    finally:
-        m = 0
-        for i in p:
-            m += 1
-        print()
-        return (m)
+    c = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            c += 1
+        except IndexError:
+            break
+    print()
+    return c
