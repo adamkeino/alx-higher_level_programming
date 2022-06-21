d54d735708e0883d8eabb3fd5126ce605b5ea3d4
Adam Keino <keinoadam@gmail.com>
2022-06-21 16:38:52 +0000

    nested for

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -4,13 +4,16 @@
 
 class Square:
     """Represents a square
+
     Attributes:
         __size (int): size of a side of the square
     """
     def __init__(self, size=0):
         """initializes the square
+
         Args:
             size (int): size of a side of the square
+
         Returns:
             None
         """
@@ -18,6 +21,7 @@ class Square:
 
     def area(self):
         """calculates the square's area
+
         Returns:
             The area of the square
         """
@@ -37,10 +41,10 @@ class Square:
         """sets the size
 
         Args:
-            value (int): the side of the square
+            value (int): side of the square
 
         Returns:
-            None
+            the size as value
         """
         if type(value) is not int:
             raise TypeError("size must be an integer")
@@ -48,3 +52,15 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
+    def my_print(self):
+        """prints out square of a character
+
+        Returns:
+            None
+        """
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+            print("".join(['#' for j in range(self.__size)]))
