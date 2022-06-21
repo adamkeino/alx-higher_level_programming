4584e050e23609e705004e906d2d8d3595030a9d
Adam Keino <keinoadam@gmail.com>
2022-06-21 12:35:59 +0000

    doc

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 class Square:
+    """This is an empty class that defines the attributes of a square.
+    It is empty for now, but in future, we will add to it more aspects.
+    """
     pass
