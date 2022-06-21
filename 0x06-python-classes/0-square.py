a332ad4372147628f1a387df1972e402e9307483
Adam Keino <keinoadam@gmail.com>
2022-06-21 12:37:14 +0000

    init

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,4 +3,5 @@ class Square:
     """This is an empty class that defines the attributes of a square.
     It is empty for now, but in future, we will add to it more aspects.
     """
-    pass
+    def __init__(self):
+        pass
