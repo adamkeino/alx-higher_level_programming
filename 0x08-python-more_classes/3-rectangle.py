f5e37e51aed00b7bf705b3a7b768f96a92c15ed2
Adam Keino <keinoadam@gmail.com>
2022-06-27 12:52:15 +0000

    empty?

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -83,4 +83,6 @@ class Rectangle:
         string = ""
         if self.__width != 0 and self.__height != 0:
             string += "\n".join("#" * self.__width for j in range(self.__height))
+        else:
+            string = ""
         return string
