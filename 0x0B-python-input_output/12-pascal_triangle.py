f4c7cc2aa05cdce8e39f054f69c71f4fa8878252
Adam Keino <keinoadam@gmail.com>
2022-07-05 18:41:15 +0000

    py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -5,8 +5,8 @@ Function that constructs the pascal triangle
 
 
 def pascal_triangle(n):
+    """appends pascals to a list"""
     la = []
     for i in range(n):
         la.append(''.join(map(str, str(11**i))))
-    
     return (la)
