0a795c4471909e1057732040dd55323cd6d468ee
Adam Keino <keinoadam@gmail.com>
2022-07-05 17:44:34 +0000

    ?

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,6 +9,8 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 filename = "add_item.json"
+
+json_list = []
 json_list = load_from_json_file(filename)
 
 for arg in argv[1:]:
