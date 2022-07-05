4d9071d4dadd2d2035cc31a65a58586c7469434d
Adam Keino <keinoadam@gmail.com>
2022-07-05 16:31:11 +0000

    read

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a reading function"""
+
+
+def read_file(filename=""):
+    """reads files"""
+    with open(filename, mode="r", encoding="utf-8") as mefile:
+        print(mefile.read())
