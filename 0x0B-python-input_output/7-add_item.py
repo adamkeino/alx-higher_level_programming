64df698953b9d52d7e0f16815062048b63af259f
Adam Keino <keinoadam@gmail.com>
2022-07-05 17:40:43 +0000

    ha

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,11 +9,7 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 filename = "add_item.json"
-
-try:
-    json_list = load_from_json_file(filename)
-except *:
-    json_list = []
+json_list = load_from_json_file(filename)
 
 for arg in argv[1:]:
     json_list.append(arg)
