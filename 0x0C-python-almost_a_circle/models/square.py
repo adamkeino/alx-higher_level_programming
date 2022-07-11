642d92f5e9e7e1d5fd963650f0b2788b0dfd1fe8
Adam Keino <keinoadam@gmail.com>
2022-07-11 11:54:10 +0000

    square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""contains class square"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """initiates the class"""
+        self.size = size
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """getter for side of square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setter for side of square"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """string representation of the square, informal"""
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+                self.x, self.y, self.width)
